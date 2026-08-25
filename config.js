f80a1aba3bb036bdcc6ad870ab651491e5c095c5
lightrune <lightrune@gmail.com>
2026-08-25 13:42:53 +0900

    End V2 web test: update config.js

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 window.POL_MEDIC_PORTAL_CONFIG = Object.freeze({
   v2: {
-    enabled: true,
-    url: "https://pol-medic-v2060-pilot.lightrune.chatgpt.site/",
+    enabled: false,
+    url: "",
   },
   v3: {
     enabled: true,
     url: "https://pol-medic-v2060-pilot.lightrune.chatgpt.site/?version=v3",
   },
 });
+
